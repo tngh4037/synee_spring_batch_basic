15a0a80a9cee2afca913cebf9ddf1a375e488f8f
tngh4037 <tngh4037@gmail.com>
2026-04-22 11:09:56 +0900

    init

---
+++ src/main/java/com/system/batch/sybatchsystem/SyBatchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.batch.sybatchsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyBatchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyBatchSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/batch/sybatchsystem/SyBatchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.batch.sybatchsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyBatchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
